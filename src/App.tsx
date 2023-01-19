5213d77a26106faa8829f1311163c7df59325a11
okaryo <okaryotr@gmail.com>
2023-01-19 23:40:31 +0900

    :sparkles: add property to svg

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ import satori from 'satori'
 async function App() {
   const font = fs.readFileSync('./src/assets/Roboto-Medium.ttf')
   const svg = await satori(
-    <div style={{ color: 'black' }}>hello, world</div>,
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',
+        color: 'black',
+        backgroundColor: 'green'
+      }}
+    >
+      hello, world
+    </div>,
     {
       width: 600,
       height: 400,
